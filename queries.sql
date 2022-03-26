871afb7a86153da9b1caaae9eedbb44cb78030d4
Amanda Johnson <lambdamanda@gmail.com>
2022-03-26 15:02:34 -0500

    Assignment #4 completed

--- queries.sql
+++ queries.sql
@@ -7,3 +7,4 @@
 ## Part 3: Test it with SQL
 -- drop table job
 ## Part 4: Test it with SQL
+-- select distinct name, description from skill inner join job_skills on skill.id = job_skills.skills_id order by name asc
